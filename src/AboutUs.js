5c74d290f4d3a0ee799602e904383a831c4fa543
Harshit Joshi <harshit54@rocketmail.com>
2021-12-09 10:55:45 +0530

    Added .env support

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -13,9 +13,8 @@ export default function imageSR() {
                 <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                 <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/204x204" />
                 <div class="flex-grow sm:pl-8">
-                    <h2 class="title-font font-medium text-lg text-gray-900">Atticus Finch</h2>
-                    <h3 class="text-gray-500 mb-3">UI Developer</h3>
-                    <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                    <h2 class="title-font font-medium text-lg text-gray-900">Harshit Joshi</h2>
+                    <h3 class="text-gray-500 mb-3">App Development</h3>
                     <span class="inline-flex">
                     <a class="text-gray-500">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
@@ -40,9 +39,8 @@ export default function imageSR() {
                 <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                 <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/206x206" />
                 <div class="flex-grow sm:pl-8">
-                    <h2 class="title-font font-medium text-lg text-gray-900">Henry Letham</h2>
-                    <h3 class="text-gray-500 mb-3">Designer</h3>
-                    <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                    <h2 class="title-font font-medium text-lg text-gray-900">Ayush Singh</h2>
+                    <h3 class="text-gray-500 mb-3">AI Model Development </h3>
                     <span class="inline-flex">
                     <a class="text-gray-500">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
--- src/ImageSR.js
+++ src/ImageSR.js
@@ -4,9 +4,9 @@ export default function imageSR(props) {
   return(
     <div class="row">
       <h1 class="sm:text-3xl font-medium text-gray-900 pt-10">Result</h1>
-      <div class="column pt-0">
+      <div class="column shadow-sm rounded-lg mt-10">
       <section class="text-gray-600 body-font">
-          <div class="container mx-auto flex px-5 py-12 items-center justify-center flex-col">
+          <div class="container mx-auto flex items-center justify-center flex-col">
             <img class="lg:w-4/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={`./bank-data/images/${props.inputFileName}`}/>
             <div class="text-center lg:w-2/3 w-full">
               <h2 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Original</h2>
@@ -15,9 +15,9 @@ export default function imageSR(props) {
           </div>
         </section>
       </div>
-      <div class="column pt-0">
+      <div class="column shadow-sm rounded-lg">
         <section class="text-gray-600 body-font">
-          <div class="container mx-auto flex px-5 py-12 items-center justify-center flex-col">
+          <div class="container mx-auto flex mt-10 items-center justify-center flex-col">
             <img class="lg:w-5/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={'./bank-data/images/output.jpg'}/>
             <div class="text-center lg:w-2/3 w-full">
               <h2 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Super-Scaled</h2>
